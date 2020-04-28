6540df3c1e1cc0cc3afa584bf222358b866c37ff
pathas <pathas@naver.com>
2020-04-28 21:21:03 +0900

    deploying

---
+++ build/precache-manifest.a057044884655f28eefc8fadf78185a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3216482d30b4e8365e7f5a131334476",
+    "url": "./index.html"
+  },
+  {
+    "revision": "229c661b4661870c5147",
+    "url": "./static/css/main.97522a21.chunk.css"
+  },
+  {
+    "revision": "6a5646ba6478cd05de1e",
+    "url": "./static/js/2.7353b672.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7353b672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229c661b4661870c5147",
+    "url": "./static/js/main.7296b36b.chunk.js"
+  },
+  {
+    "revision": "90f3579df1dcbcb9c551",
+    "url": "./static/js/runtime-main.cc2dbf65.js"
+  },
+  {
+    "revision": "55e1764ac78314aad18ed3cba90868e3",
+    "url": "./static/media/NotoSans-Light.55e1764a.ttf"
+  },
+  {
+    "revision": "82967d53f162f2751ce29c6e33bad3ec",
+    "url": "./static/media/NotoSansCJKkr-DemiLight.82967d53.otf"
+  }
+]);
